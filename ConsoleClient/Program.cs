524e66354b1a80db0608ea4f8e3b020638a8ed68
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-02-06 08:29:05 -0600

    first commit

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsoleClient.ServiceAcademy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCFvsAPI1;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Service1Client Ser = new Service1Client();       
+
+            //Console.WriteLine(Ser.GetData(5));
+
+            //CompositeType T = new CompositeType();
+            //T.BoolValue = true;
+            //T.StringValue = "FASV1";
+
+
+            //CompositeType resp = Ser.GetDataUsingDataContract(T);
+
+            //Console.WriteLine(resp.StringValue + " " + resp.BoolValue);          
+
+            string palabra2 = "";
+            foreach (var item in Ser.BuscarLista(palabra2))
+            {
+                Console.WriteLine(item.Snippet.Title);
+                Console.WriteLine(item.Id.VideoId);              
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
